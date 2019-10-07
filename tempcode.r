8507a2e22656a602f3416c487d4f2fdfc859a68e
cjsulzer <csulz814@uwsp.edu>
2019-10-07 17:37:30 -0500

    rev3

--- tempcode.r
+++ tempcode.r
@@ -3,11 +3,7 @@ WVSData <- read.csv(file="C:/Users/csulz814/OneDrive - UWSP/DAC 299/WV_US.csv",
 summary(WVSData$V23)
 str(WVSData)
 myReg = lm(V23~V59,WVSData)
-
 summary(myReg)
-
 summary(WVSData$V23)
 hist(WVSData$V23)
-
-summary(myReg)
 
