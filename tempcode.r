df05b536223286aae8fec24481c621f505186c7d
Sulzer <csulz814@uwsp.edu>
2019-10-07 17:20:09 -0500

    Initial regression of WV survey

---
+++ tempcode.r
@@ -0,0 +1,6 @@
+WVSData <- read.csv(file="C:/Users/csulz814/OneDrive - UWSP/DAC 299/WV_US.csv",
+                    header=TRUE, sep=",")
+summary(WVSData$V23)
+str(WVSData)
+myReg = lm(V23~V59,WVSData)
+summary(myReg)
